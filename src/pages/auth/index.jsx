eaf036090a5bc7fa8d8414d7890c668050d36640
pista <attila.morocz02@gmail.com>
2024-01-14 12:47:08 +0100

    fixed

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import { auth, provider } from "../../config/firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate, Navigate } from "react-router-dom";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import "./style.css";
 
 export const Auth = () => {
+  const navigate = useNavigate();
+  const {isAuth} = useGetUserInfo();
+
+  const signInWithGoogle = async () => {
+    const results = await signInWithPopup(auth, provider);
+    const authInfo = {
+      userID: results.user.uid,
+      name: results.user.displayName,
+      profilePhoto: results.user.photoURL,
+      isAuth: true,
+    };
+    localStorage.setItem("auth", JSON.stringify(authInfo));
+    navigate("expenses");
+  };
+
+  if (isAuth) {
+    return <Navigate to="/expenses" />
+  }
+
   return (
-    <div>index.auth</div>
-  )
-}
+    <div className="login-page">
+      <p>Sign in with Google to continue</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        {" "}
+        Sign in with Google
+      </button>
+    </div>
+  );
+};
